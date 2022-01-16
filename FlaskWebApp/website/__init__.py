1ced86f919f8955688a6ae6baf6d7e07ed89f10f
M Ali Akbar Sinaga <akbar081200@gmail.com>
2022-01-16 15:14:43 +0700

    Membuat Halaman dan Fitur Absensi

--- FlaskWebApp/website/__init__.py
+++ FlaskWebApp/website/__init__.py
@@ -11,13 +11,16 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
     from .views import views
     from .auth import auth
+    # from .absensi import absensi
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+    # app.register_blueprint(absensi, url_prefix='/')
 
     from .models import User, Note
 
